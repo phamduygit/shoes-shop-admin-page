24cc7defc8990a23605c86309082cd3075fe2c02
phamduygit <minhduy.bthuy@gmail.com>
2023-05-04 20:53:49 +0700

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Dashboard from './dashboard/DashBoard';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Dashboard />
   </React.StrictMode>
 );
 
